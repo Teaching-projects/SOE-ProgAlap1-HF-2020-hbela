4d02741db11ad85e47376c381429938890867015
dxtr2000 <hbela2000@gmail.com>
2020-10-25 00:32:23 +0200

    Kész.

--- 008/main.py
+++ 008/main.py
@@ -1,13 +1,31 @@
+import math
+
 x=0.0
 y=0.0
 
+irany = str(input())
+if irany == "stop":
+    print(round(x,2))
+    print(round(y,2))
+    print(math.sqrt(pow((0-round(x,2)),2) + pow((0-round(y,2)),2)))
+else:
+    egyseg = float(input())
+    while irany != 'stop':
+        if irany == "forward":
+            y += egyseg
+        if irany == "backward":
+            y -= egyseg
+        if irany == "right":
+            x += egyseg
+        if irany == "left":
+            x -= egyseg
+        irany = str(input())
+        if irany == "stop":
+            break
+        else:
+            egyseg = float(input())
+    print(round(x,2))
+    print(round(y,2))
+    print(math.sqrt(pow((0-round(x,2)),2) + pow((0-round(y,2)),2)))
 
-
-
-
-
-
-
-print(round(x,2))
-print(round(y,2))
     
