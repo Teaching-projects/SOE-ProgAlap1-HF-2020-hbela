0102457a8a996c8fbd9a0601d2c05146df7aba79
dxtr2000 <hbela2000@gmail.com>
2020-10-27 08:08:17 +0100

    Kész.

--- 010/main.py
+++ 010/main.py
@@ -1,11 +1,22 @@
-
-
-
-
+honap = 0
+total = 0
+perc = []
+sms = []
 szamla=[]
 
+while honap != 12:
+    perc.append(int(input()))
+    sms.append(int(input()))
+    honap += 1
+cshavi = int(input())
+csperc = int(input())
+csSMS = int(input())
 
-
+for i in range(len(perc)):
+    if perc[i]*csperc + sms[i]*csSMS < cshavi: szamla.append(cshavi)
+    else: szamla.append(perc[i]*csperc + sms[i]*csSMS)
+for i in range(len(szamla)):
+    total += szamla[i]
 
 print(szamla)
 print(total)
